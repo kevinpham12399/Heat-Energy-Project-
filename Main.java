3b40fc42b44c080d9bc499cc8a9ede2db550ddba
kevinpham12399 <69619326+kevinpham12399@users.noreply.github.com>
2020-09-16 17:31:08 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,20 @@
 import java.util.Scanner; 
+
 public class Main { 
-public static void main(String[] args) { 
-Scanner reader = new Scanner(System.in); 
-System.out.println("Enter the mass of the water, in grams"); 
-double mass = reader.nextDouble(); 
-System.out.println("Enter the intial temperature of the water, in  Celsius"); 
-double initialTemp = reader.nextDouble(); 
-if(initialTemp < -273.14) 
-initialTemp = -273.14; 
-System.out.println("Enter the final temperature of the water, in  Celsius"); 
+
+    public static void main(String[] args) { 
+
+        Scanner reader = new Scanner(System.in); 
+
+        System.out.println("Enter the mass of the water, in grams"); 
+        double mass = reader.nextDouble(); 
+        System.out.println("Enter the intial temperature of the water, in  Celsius"); 
+
+            double initialTemp = reader.nextDouble(); 
+            if(initialTemp < -273.14) 
+            initialTemp = -273.14; 
+            System.out.println("Enter the final temperature of the water, in  Celsius"); 
+
 double finalTemp = reader.nextDouble(); 
 if(finalTemp < -273.14) 
 finalTemp = -273.14; 
